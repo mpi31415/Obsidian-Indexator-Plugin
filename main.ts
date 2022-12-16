d64455c8f3062775673abee770915e84eb0374ab
max <maximilian.ponomarev@gmail.com>
2022-12-16 09:54:46 +0100

    Version updated

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, MarkdownEditView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, Vault, View, Workspace, WorkspaceLeaf, TextFileView, TFolder } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -12,21 +12,23 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
-
+	myVault: Vault;
 	async onload() {
 		await this.loadSettings();
+		this.myVault = app.vault;
 
+	
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Indexator', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('This is a now tice!');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('Status Bat Text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -41,7 +43,7 @@ export default class MyPlugin extends Plugin {
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
+			//	console.log(editor.getSelection());
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
@@ -71,7 +73,7 @@ export default class MyPlugin extends Plugin {
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+			//console.log('click', evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
@@ -90,7 +92,11 @@ export default class MyPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
-
+class myVault extends Vault{
+	constructor(){
+		super();
+	}
+}
 class SampleModal extends Modal {
 	constructor(app: App) {
 		super(app);
@@ -107,6 +113,70 @@ class SampleModal extends Modal {
 	}
 }
 
+function findIndexatorFile(searchPath: String){
+	let allFiles = this.app.vault.getFiles();
+						//console.log(allFiles.length);
+						for (let i = 0; i<allFiles.length; i++){
+							
+							if(allFiles[i].path==searchPath){
+								return allFiles[i];
+				
+								break;
+							}
+						}
+}
+
+function closeOpenIndexatorLeaves(){
+	this.app.workspace.iterateAllLeaves((leaf: WorkspaceLeaf)=>{
+		//console.log(leaf.getViewState().type);
+		if(leaf.getViewState().type == "markdown"){
+			//console.log(leaf.getDisplayText());
+			if(leaf.getDisplayText()=="Indexator"){
+				leaf.detach();
+			}
+		}
+	})
+}
+function findAllTopLevelFolders(folder:TFolder){
+	let folders: string[] = [];
+	for(let i = 0; i<folder.children.length;i++){
+
+		if(folder.children[i] instanceof TFolder){
+			folders.push(folder.children[i].name);
+			console.log(folder.children[i].name);
+			
+		}
+	}
+	return folders;
+}
+
+function findAllFolders(){
+	let allFolders: string[] = [];
+	let root = this.app.vault.getRoot();
+	let allTPF = findAllTopLevelFolders(root);
+	for(let f of allTPF){
+		allFolders.push(f);
+
+	}
+
+	return allFolders;
+}
+function initializeIndexatorFile(file:TFile, baseName: string){
+	this.app.vault.modify(file,"");
+	//writes
+	this.app.vault.append(file,"Untick all folders which you want to be indexed \n ______ \nimportant Note: \nfor a file to be indexed, it requires at least one tag(files with multiple tags, will be indexed multiple times), else the file will be found under \"unindexed\" \n_____\n").then(()=>{});	
+	//adds folders
+
+	let allF = findAllFolders();
+	for(let fol of allF){
+		if(fol!=baseName){
+			this.app.vault.append(file, "- [X] "+fol+"\n");
+		}
+	
+	}
+}
+
+
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
@@ -120,18 +190,57 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Indexator.'});
+
+	
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Set your indexation-file')
+			.setDesc('Create or choose a new file')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
+				.setPlaceholder('A new folder, with this name will be created')
 				.setValue(this.plugin.settings.mySetting)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
+					//alert('Secret: ' + value);
 					this.plugin.settings.mySetting = value;
 					await this.plugin.saveSettings();
-				}));
+				}))
+				.addButton(bt => bt
+					.setButtonText('Save').onClick(async ()=>{
+
+					this.app.vault.createFolder(this.plugin.settings.mySetting).catch(()=>{
+						alert("Such a folder already exists: "+this.plugin.settings.mySetting);
+					});
+
+					let exists = false;
+					let indexPath = this.app.vault.getRoot().path+"/"+this.plugin.settings.mySetting+"/"+"Indexator.md";
+					let searchPath = this.plugin.settings.mySetting+"/"+"Indexator.md";
+					
+					let file = await this.app.vault.create(indexPath,"").catch(()=>{
+						exists = true;
+						alert("Such a file aready exsists");
+					}).then();
+
+					if(exists){
+						file = findIndexatorFile(searchPath);
+					}
+				
+					closeOpenIndexatorLeaves();
+
+					if (file instanceof TFile){
+					
+						let leaf = this.app.workspace.getLeaf(true);
+						leaf.openFile(file);
+						//creates file for the first time
+						if(!exists){
+						 initializeIndexatorFile(file, this.plugin.settings.mySetting);
+						}
+						
+
+
+					}
+				}
+					 
+		));
 	}
 }

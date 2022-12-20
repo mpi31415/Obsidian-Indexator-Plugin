140d8eb0796e81a3107c45698b366de430d338f1
max <maximilian.ponomarev@gmail.com>
2022-12-20 13:57:22 +0100

    rework of process, migration to settingstab, removal of Indexator.md file

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
-import { App, Plugin, PluginSettingTab,Vault} from 'obsidian';
+import { App, Plugin, PluginSettingTab,Setting,TFile,TFolder,Events,Vault} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+
+
 
 interface IndexatorSettings {
-	path: string
+	path: string,
+	fileName: string
 }
 
 const DEFAULT_SETTINGS: IndexatorSettings = {
-	path: "Index"
-	
+	path: "Index",	
+	fileName:"Indexator"
 }
+
+let CURRENT_SETTINGS: IndexatorSettings = DEFAULT_SETTINGS;
 
 export default class MyPlugin extends Plugin {
 	settings: IndexatorSettings;
@@ -17,11 +21,10 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 		this.myVault = app.vault;
+		let root = this.app.vault.getRoot();
+		
 
 		this.addSettingTab(new IndexatorSettingTab(this.app, this))
-
-		//locate indexator.md file -> if not exists create one 
-
 
 		}
 
@@ -37,7 +40,28 @@ export default class MyPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
+
+
+
+function getAllFolders(folder: TFolder){
+	let allFolders: TFolder[] = new Array<TFolder>;
+	folder.children.forEach((file)=>{
+		if(file instanceof TFolder){
+			allFolders.push(file);
+		}
+	 });
+	 return allFolders;
+}
+
+
+function index(folderStates: Map<TFolder,boolean>){
+	folderStates.forEach((state, fol)=>{
+		if(state)
+			app.vault.create(fol.path+"/Index.md","");
+	});
+}
 class IndexatorSettingTab extends PluginSettingTab {
+	
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -46,5 +70,37 @@ class IndexatorSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
+		let { containerEl } = this;
+		let allFolders = getAllFolders(this.app.vault.getRoot());
+		let foldersStates: Map<TFolder, boolean> = new Map<TFolder,boolean>;
+		for (let fol of allFolders){
+			foldersStates.set(fol, true);
+		}
+		
+		containerEl.empty();
+		
+		containerEl.createEl("h2",{text:"Folders to be indexed, untick all those which are not supposed to be indexed"});
+		for(let a of allFolders){
+			new Setting(containerEl)
+				.setName(a.name)
+				.setClass("checked")
+				.addToggle((cb)=>{
+					cb.setValue(true);
+					cb.onChange((val)=>{
+						foldersStates.set(a, val);
+					})
+			});	
+		}
+		
+		new Setting(containerEl).addButton((bt)=>{
+			bt.setButtonText("Index");
+			bt.onClick(()=>{
+				index(foldersStates);
+			});
+		});		
+	
+		containerEl.show();
+
+	
 	}
 }
